4e48a0fc732c486efe4e8adcbbf1172afe33149c
Junior Oliveira <arojunior@gmail.com>
2017-08-21 08:27:23 -0300

    should include the title of each item

--- src/__tests__/LovableFilterableTable.test.js
+++ src/__tests__/LovableFilterableTable.test.js
@@ -24,5 +24,35 @@ describe('LovableFilterableTable', () => {
       expect(wrapper.find('tbody > tr').exists()).toBe(false)
     })
   })
-  
+
+  describe('when given some `items`', () => {
+    // Presence in this array does not indicate endorsement
+    const items = [
+      { id: 1, name: 'Bitcoin' },
+      { id: 2, name: 'Ethereum' },
+      { id: 3, name: 'Litecoin' }
+    ]
+
+    beforeEach(() => {
+      wrapper = shallow(
+        <LovableFilterableTable items={items} schema={tableSchema} />
+      )
+    })
+
+    it('should render corresponding number of table rows', () => {
+      expect(wrapper.find('tbody > tr').length).toEqual(3)
+    })
+
+    it('should include the title of each item', () => {
+      items.forEach(item => {
+        expect(
+          wrapper.containsMatchingElement(
+            <td>
+              {item.name}
+            </td>
+          )
+        ).toBe(true)
+      })
+    })
+  })
 })
